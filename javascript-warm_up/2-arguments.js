2a33547759a87ce5d5b39fc1d75f3fc5d2271344
Branis333 <b.sumba@alustudent.com>
2024-02-01 10:57:38 +0000

    Warm up

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 
-if (argv.length === 0) {
+if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
   console.log('Arguments found');
